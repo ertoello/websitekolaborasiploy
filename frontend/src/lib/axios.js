00d70811fab4ede45df3ebe63679df5a3e7b8020
ertoello <ertoello17fx@gmail.com>
2025-05-03 13:48:19 +0700

    test deploy 2

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,8 +4,8 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? // ? "https://w3lc3pgc-5000.asse.devtunnels.ms/api/v1" // kalau lagi testing online (DevTunnel)
-        "https://poetic-stillness-production-59fc.up.railway.app/api/v1" // atau ini kalau lokal
-      : "https://website-kolaborasi-production.up.railway.app/api/v1",
+        "http://localhost:5000/api/v1" // atau ini kalau lokal
+      : "https://poetic-stillness-production-59fc.up.railway.app/api/v1",
   withCredentials: true,
 });
 
