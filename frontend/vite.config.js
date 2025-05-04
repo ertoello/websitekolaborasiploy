5096d60069b0d1fe7fc6bace18cefc74dc7090da
ertoello <ertoello17fx@gmail.com>
2025-05-04 18:45:43 +0700

    test deploy 21

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: "esnext",
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  optimizeDeps: {
+    // Gabungkan semua dependensi yang perlu di-optimize dalam satu array
+    include: ["@emotion/is-prop-valid", "quill", "react-quilljs"],
+  },
+});
