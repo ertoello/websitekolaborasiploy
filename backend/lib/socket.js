2ffb902377157ac1741338af5af5498a4fb89722
ertoello <ertoello17fx@gmail.com>
2025-05-04 22:57:11 +0700

    test deploy 32

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -5,15 +5,18 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
+const allowedOrigins =
+  process.env.NODE_ENV === "production"
+    ? ["https://websitekolaborasiploy.vercel.app"]
+    : ["http://localhost:5173"];
+
 const io = new Server(server, {
   cors: {
-    origin: [
-      // "https://websitekolaborasiploy-production.up.railway.app",
-      "https://websitekolaborasiploy.vercel.app",
-    ],
+    origin: allowedOrigins,
     credentials: true,
   },
 });
+
 
 const userSocketMap = {}; // { userId: socketId }
 
